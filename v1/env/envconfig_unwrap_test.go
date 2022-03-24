a6a816bf8f0475f5fb5eb0b1afdf6ff474db39ea
Brian W. Wolter <brian@brianwolter.com>
2022-03-24 17:32:53 -0400

    ...

--- v1/env/envconfig_unwrap_test.go
+++ v1/env/envconfig_unwrap_test.go
@@ -1,18 +1,15 @@
 package env
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"testing"
 
+	"github.com/bww/go-config/v1/secrets"
 	"github.com/bww/go-config/v1/secrets/staticsecrets"
 	"github.com/stretchr/testify/assert"
 )
-
-var secretPostproc = staticsecrets.New(map[string]string{
-	"value_1": "unwrapped_value_1",
-	"value_2": "unwrapped_value_2",
-})
 
 type SpecificationWithUnwrap struct {
 	Value1 string `env:"VALUE_1" unwrap:"secret"`
@@ -22,17 +19,43 @@ type SpecificationWithUnwrap struct {
 
 func TestProcessUnwrap(t *testing.T) {
 	var s SpecificationWithUnwrap
+
 	os.Clearenv()
 	os.Setenv("ENV_VALUE_1", "value_1")
 	os.Setenv("ENV_VALUE_2", "value_2")
 	os.Setenv("ENV_VALUE_3", "value_3")
 
 	p := &Processor{}
-	p.RegisterSecrets(secretPostproc)
+	p.RegisterSecrets(staticsecrets.New(map[string]string{
+		"value_1": "unwrapped_value_1",
+		"value_2": "unwrapped_value_2",
+	}))
+
 	err := p.Process("env", &s)
 	assert.Nil(t, err, fmt.Sprint(err))
 
 	assert.Equal(t, "unwrapped_value_1", s.Value1)
 	assert.Equal(t, "unwrapped_value_2", s.Value2)
 	assert.Equal(t, "value_3", s.Value3)
+}
+
+func TestProcessUnwrapError(t *testing.T) {
+	var s SpecificationWithUnwrap
+
+	os.Clearenv()
+	os.Setenv("ENV_VALUE_1", "value_1")
+	os.Setenv("ENV_VALUE_2", "value_2")
+	os.Setenv("ENV_VALUE_3", "value_3")
+
+	p := &Processor{}
+	p.RegisterSecrets(staticsecrets.New(map[string]string{
+		"value_1": "unwrapped_value_1",
+		// value_2 is not present, cannot unwrap
+	}))
+
+	err := p.Process("env", &s)
+	if assert.NotNil(t, err, "Expcted an error") {
+		assert.Equal(t, true, errors.Is(err, secrets.ErrNotFound), "Expected not-found error")
+	}
+
 }
